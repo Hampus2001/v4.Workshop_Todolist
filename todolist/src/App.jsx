1a4b68e176992be6ec5fb4df709e596ba0b9b0bc
Hampus2001 <hampus.soderlund@chasacademy.se>
2025-01-21 16:45:07 +0100

    Todolist with delete button

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -26,24 +26,30 @@ function App() {
     setState(newState);
   };
 
+  const deleteTask = (id) => {
+    const newState = state.filter((item) => item.id !== id);
+    setState(newState);
+  };
+
   return (
     <div className="flex flex-col items-center">
-      <div className="m-10 flex justify-center flex-col gap-4">
+      <div className="mt-40 flex justify-center flex-col gap-4 bg-orange-50 p-10 rounded-lg">
         <div className="flex flex-row justify-center mb-10">
           <input
-            className="bg-gray-300 p-2 rounded-l-lg border-b-2 border-t-2 border-l-2 border-black"
+            className="bg-orange-100 p-2 rounded-l-lg border-b-2 border-t-2 border-l-2 border-orange-300 text-xl font-bold"
             type="text"
             placeholder="Todos?"
             onChange={handleChange}
           ></input>
           <button
-            className="bg-gray-300 p-2 rounded-r-lg border-2 border-black"
+            className="bg-orange-100 p-2 rounded-r-lg border-2 border-orange-300 text-xl font-bold
+            "
             onClick={printTodo}
           >
             Add
           </button>
         </div>
-        <div className="bg-blue-300 flex flex-col rounded-lg items-end ">
+        <div className="bg-orange-100 flex flex-col rounded-lg items-center border-8 border-orange-300 shadow-lg">
           {state.map(function (object) {
             return (
               <TodoItemComponent
@@ -51,6 +57,7 @@ function App() {
                 title={object.title}
                 completed={object.completed}
                 handleStatusProp={handleStatus}
+                deleteTask={deleteTask}
               />
             );
           })}
--- todolist/src/Components/TodoItemComponent.jsx
+++ todolist/src/Components/TodoItemComponent.jsx
@@ -1,21 +1,27 @@
 import { CgCloseO } from "react-icons/cg";
 import { CgCheckO } from "react-icons/cg";
+import { CgTrash } from "react-icons/cg";
 
-export default function ({ id, title, completed, handleStatusProp }) {
+export default function ({
+  id,
+  title,
+  completed,
+  handleStatusProp,
+  deleteTask,
+}) {
   return (
-    <div className="flex flex-row gap-2 justify-center items-center text-4xl">
+    <div className="flex flex-row justify-between items-center text-center text-4xl p-4 w-96">
       <div key={id}>{title}</div>
-      {completed ? (
-        <CgCheckO
-          className="text-4xl"
-          onClick={() => handleStatusProp(id, completed)}
-        />
-      ) : (
-        <CgCloseO
-          className="text-4xl"
-          onClick={() => handleStatusProp(id, completed)}
-        />
-      )}
+      <div className="flex gap-3">
+        {completed ? (
+          <CgCheckO onClick={() => handleStatusProp(id, completed)} />
+        ) : (
+          <CgCloseO onClick={() => handleStatusProp(id, completed)} />
+        )}
+        <button onClick={() => deleteTask(id)}>
+          <CgTrash />
+        </button>
+      </div>
     </div>
   );
 }
